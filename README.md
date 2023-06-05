d1554dea38018a2ceb9e3a0c1ccd8b927e78086f
Emerson Espinoza <xinayespinoza@gmail.com>
2023-06-04 21:51:10 -0400

    conectando nuestra simulación de base de datos y creando rutas

---
+++ README.md
@@ -0,0 +1,14 @@
+# Rest Api con flask 
+para levanta el proyecto 
+```bash 
+git clone 
+```
+```bash
+python3 app.py 
+```
+
+## Recordando para que sirven 
+```py 
+from flask import Flask, jsonify
+```
+el jsonify es para crear string en json 
--- app.py
+++ app.py
@@ -1,5 +1,18 @@
-from flask import Flask
+from products import products
+from flask import Flask, jsonify
 app = Flask(__name__)
+
+# creando las rutas
+
+
+@app.route('/ping')
+def ping():
+    return jsonify({"message": "pong"})
+
+
+@app.route('/products')
+def getProducts():
+    return jsonify({"products": products, "message": "Products List"})
 
 
 if __name__ == '__main__':
