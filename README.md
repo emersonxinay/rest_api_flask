f4502a0cf609a375e6debd6d6e705ea7a65f16ba
Emerson Espinoza <xinayespinoza@gmail.com>
2023-06-04 23:14:57 -0400

    rutas y img para el readme

--- README.md
+++ README.md
@@ -9,6 +9,68 @@ python3 app.py
 
 ## Recordando para que sirven 
 ```py 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 ```
 el jsonify es para crear string en json 
+y el request para peticiones REST
+## Los endpoints para usar esta api 
+## para mostrar todos los productos - get
+```route
+http://localhost:3000/products
+``` 
+### se debería mostra asi: 
+<img src="./img/todos_productos.png">
+
+
+## para mostrar un solo producto - get
+```route
+http://127.0.0.1:3000/products/maus
+```
+### se debería mostra asi: 
+<img src="./img/detalle_producto.png">
+
+## para agregar un producto - post
+```route 
+http://127.0.0.1:3000/products
+```
+seguido desde el postman o imsomia, ingresar los datos con esta estructura 
+```json 
+{
+	"name": "maus game",
+	"price": "500",
+	"quantity": 5
+}
+```
+### se debería mostra asi: 
+<img src="./img/producto_agregad.png">
+
+## para actualizar producto - put
+```route
+http://127.0.0.1:3000/products/cpu
+```
+seguido desde el postman o imsomia, ingresar los datos con esta estructura
+```json
+{
+	"name": "cpu2222",
+	"price": "400",
+	"quantity": 5
+}
+```
+### se debería mostra asi: 
+<img src="./img/producto_actualizado.png">
+
+## para eliminar un producto - delete 
+```route
+http://127.0.0.1:3000/products/maus
+```
+seguido desde el postman o imsomia desde la ruta el nombre del producto que quiere eliminar 
+### se debería mostra asi: 
+<img src="./img/producto_eliminado.png">
+
+
+
+
+
+
+
+
--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from products import products
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 # creando las rutas
@@ -23,6 +23,46 @@ def getProduct(product_name):
     if (len(productsFound) > 0):
         return jsonify({"product": productsFound[0]})
     return jsonify({"message": "producto not found"})
+
+# crear productos
+
+
+@app.route('/products', methods=['POST'])
+def add_product():
+    new_product = {
+        "name": request.json['name'],
+        "price": request.json['price'],
+        "quantity": request.json['quantity']
+    }
+    products.append(new_product)
+    return jsonify({"message": "product added succesfully ", "products": products})
+
+# actualizar o editar datos
+
+
+@app.route('/products/<string:product_name>', methods=['PUT'])
+def editProduct(product_name):
+    productFound = [
+        product for product in products if product['name'] == product_name]
+    if (len(productFound) > 0):
+        productFound[0]['name'] = request.json['name']
+        productFound[0]['price'] = request.json['price']
+        productFound[0]['quantity'] = request.json['quantity']
+        return jsonify({"message": "producto actualizado", "product": productFound[0]})
+
+    return jsonify({"message": "product not found"})
+
+# ruta para eliminar producto
+
+
+@app.route('/products/<string:product_name>', methods=['DELETE'])
+def deleteProduct(product_name):
+    productsFound = [
+        product for product in products if product['name'] == product_name]
+    if (len(productsFound) > 0):
+        products.remove(productsFound[0])
+        return jsonify({"message": "Product Deleted", "products": products})
+    return jsonify({"message": "Not exist product"})
 
 
 if __name__ == '__main__':
