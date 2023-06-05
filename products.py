c2208358b0d23b73888c87babe92ff6226c89b53
Emerson Espinoza <xinayespinoza@gmail.com>
2023-06-04 21:34:19 -0400

    iniciando nuestro servidor api con flask

---
+++ products.py
@@ -0,0 +1,7 @@
+products = [
+    {"name": "laptop", "price": 800, "quantity": 4},
+    {"name": "maus", "price": 40, "quantity": 10},
+    {"name": "monitor", "price": 400, "quantity": 3},
+    {"name": "cpu", "price": 1800, "quantity": 2}
+
+]
