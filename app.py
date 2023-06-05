2059d97dfc1ad2f227429c55d498d74ea86f47af
Emerson Espinoza <xinayespinoza@gmail.com>
2023-06-04 22:02:06 -0400

    rutas especifica y consultado cada dato con su id

--- app.py
+++ app.py
@@ -13,6 +13,16 @@ def ping():
 @app.route('/products')
 def getProducts():
     return jsonify({"products": products, "message": "Products List"})
+# haciendo ruta dinamica
+
+
+@app.route('/products/<string:product_name>')
+def getProduct(product_name):
+    productsFound = [
+        product for product in products if product['name'] == product_name]
+    if (len(productsFound) > 0):
+        return jsonify({"product": productsFound[0]})
+    return jsonify({"message": "producto not found"})
 
 
 if __name__ == '__main__':
